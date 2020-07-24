73e587a119da49954544f778139607dc717c8872
xurunfei <1194289919@qq.com>
2020-07-24 21:48:30 +0800

    init

---
+++ commitImagetmp.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /Users/xurunfei/.bash_profile
+cd '/Users/xurunfei/Documents/work/project/person/imagetmp'
+git add .
+git commit -am 'update'
+git pull origin master 
+git push origin master
+
